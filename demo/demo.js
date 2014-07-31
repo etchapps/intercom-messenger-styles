ead098529403d95d2b0be2f343e13fd3e098dc96
Jim Ramsden <jim@etchapps.com>
2014-07-31 15:56:02 +0100

    Fleshing out
    Still a bit scruffy - not ready for production yet

---
+++ demo/demo.js
@@ -0,0 +1,9 @@
+/* Demo styles */
+
+body {
+  font-family: sans-serif;
+  background: #eee;
+  padding: 1em;
+  color: #555;
+  text-align: center;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Intercom Messenger - Custom Styles
+
+Sass variables to help you overide the default Intercom styles
